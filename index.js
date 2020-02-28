b8d5899f709a4eb148e4683294eab5c9c7bbd078
Muhammad Osama <imuhammadosama@outlook.com>
2020-02-28 21:02:13 +0500

    Logging errors were removed.

--- index.js
+++ index.js
@@ -1,46 +1,49 @@
 // Importing the packages
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 // Connection
-mongoose.connect("mongodb://localhost/comfort-zone");
+mongoose.connect('mongodb://localhost/comfort-zone', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 let db = mongoose.connection;
 
 // Checking connection
-db.once("open", () => console.log("Connected to MongoDb"));
+db.once('open', () => console.log('Connected to MongoDb'));
 
 // Checking for DB errors
-db.on("errors", err => console.log(err));
+db.on('errors', err => console.log(err));
 
 // Importing all routes
-const users = require("./routes/api/users");
-const ads = require("./routes/api/ads");
-const complains = require("./routes/api/complains");
-const funds = require("./routes/api/funds");
-const issues = require("./routes/api/issues");
-const bills = require("./routes/api/bills");
+const users = require('./routes/api/users');
+const ads = require('./routes/api/ads');
+const complains = require('./routes/api/complains');
+const funds = require('./routes/api/funds');
+const issues = require('./routes/api/issues');
+const bills = require('./routes/api/bills');
 
 // Body Parser Middleware
 app.use(express.json());
-app.use(express.urlencoded({ encoded: false }));
+// app.use(express.urlencoded({ encoded: false }));
 
 // Setting a static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Whitelisting IPs
 app.use(cors());
-var whitelist = ["http://localhost:3000"];
+var whitelist = ['http://localhost:3000'];
 
 // Setting all routers
-app.use("/api/users", users);
-app.use("/api/ads", ads);
-app.use("/api/complains", complains);
-app.use("/api/funds", funds);
-app.use("/api/issues", issues);
-app.use("/api/bills", bills);
+app.use('/api/users', users);
+app.use('/api/ads', ads);
+app.use('/api/complains', complains);
+app.use('/api/funds', funds);
+app.use('/api/issues', issues);
+app.use('/api/bills', bills);
 
 // Setting the PORT
 const PORT = process.env.PORT || 5000;
