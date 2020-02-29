53fcf4e6777113a8f5b884aae38bc0dcbd9f9ea5
Muhammad Osama <imuhammadosama@outlook.com>
2020-02-29 15:18:29 +0500

    Deleted dummy forum data.

--- client/src/database/forumIssueCategories.js
+++
@@ -1,9 +0,0 @@
-export const issueCategories = [
-  { categoryID: "1", name: "Problems" },
-  { categoryID: "2", name: "Suggestions" },
-  { categoryID: "3", name: "Emergencies" }
-];
-
-export function getIssueCategories() {
-  return issueCategories.filter(i => i);
-}
