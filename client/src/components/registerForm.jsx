8d3772ab1ddfe8b8446f6e96dbc085b7a2ce869b
Muhammad Osama <imuhammadosama@outlook.com>
2020-02-29 18:32:51 +0500

    Registeration is done on client.

--- client/src/components/registerForm.jsx
+++ client/src/components/registerForm.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify';
-import Form from './common/form';
 import Joi from 'joi-browser';
-import axios from 'axios';
-const apiEndpoint = 'http://localhost:5000/api/users/';
+import Form from './common/form';
+import * as userService from '../services/userService';
 
 class RegisterForm extends Form {
 	state = {
@@ -19,36 +16,32 @@ class RegisterForm extends Form {
 			.label('Email'),
 		password: Joi.string()
 			.required()
-			.min(8)
+			.min(5)
 			.label('Password'),
 		name: Joi.string()
 			.required()
-			.min(5)
 			.label('Name')
 	};
 
 	doSubmit = async () => {
-		const result = await axios.post(apiEndpoint, {
-			...this.state.data
-		});
-		if (result) {
-			toast.success(
-				'You are successfully registered, go to login page to sign in.'
-			);
-		} else {
-			toast.error('Oh something went wrong');
+		// Call the server
+		try {
+			await userService.register(this.state.data);
+		} catch (ex) {
+			if (ex.response && ex.response.status === 400) {
+				const errors = { ...this.state.errors };
+				errors.email = ex.response.data;
+				this.setState({ errors });
+			}
 		}
-		const data = { email: '', password: '', name: '' };
-		this.setState({ data });
 	};
 
 	render() {
 		return (
 			<div className='container my-5'>
-				<ToastContainer />
 				<h1>Register</h1>
 				<form onSubmit={this.handleSubmit}>
-					{this.renderInput('email', 'Email', 'email')}
+					{this.renderInput('email', 'Email')}
 					{this.renderInput('password', 'Password', 'password')}
 					{this.renderInput('name', 'Name')}
 					{this.renderButton('Register')}
