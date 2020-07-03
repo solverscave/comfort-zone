b82652201477e842328f3a5da126c0f052fcdd57
Muhammad Osama <imuhammadosama@outlook.com>
2020-07-03 19:50:57 +0500

    Navbar updated!

--- client/src/components/common/navbar.jsx
+++ client/src/components/common/navbar.jsx
@@ -3,98 +3,82 @@ import { Link, NavLink } from 'react-router-dom';
 import { NavDropdown } from 'react-bootstrap';
 
 const Navbar = ({ user }) => {
-	return (
-		// <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-		<nav className='navbar navbar-expand-lg navbar-cz sticky-top'>
-			<Link to='/' className='navbar-brand'>
-				<img src={require('../../assets/img/logo-01.png')} alt='' />
-			</Link>
-			<div className='collapse navbar-collapse'>
-				<div className='navbar-nav ml-auto'>
-					{/* <NavLink className='nav-item nav-link' to='/facility-corner'>
-						Facility Corner
-					</NavLink>
-					<NavLink className='nav-item nav-link' to='/garbage-tracking'>
-						Garbage Tracking
-					</NavLink>
-					<NavLink className='nav-item nav-link' to='/security'>
-						Security
-					</NavLink>
-					<NavLink className='nav-item nav-link' to='/elections'>
-						Elections
-					</NavLink>
-					<NavLink className='nav-item nav-link' to='/tender'>
-						Tender
-					</NavLink> */}
+  return (
+    // <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+    <nav className='navbar navbar-expand-lg navbar-cz sticky-top'>
+      <Link to='/' className='navbar-brand'>
+        <img src={require('../../assets/img/logo-01.png')} alt='' />
+      </Link>
+      <div className='collapse navbar-collapse'>
+        <div className='navbar-nav ml-auto'>
+          {!user && (
+            <React.Fragment>
+              <NavLink className='nav-item nav-link' to='/login'>
+                Login
+              </NavLink>
+              <NavLink className='nav-item nav-link' to='/register'>
+                Register
+              </NavLink>
+            </React.Fragment>
+          )}
+          {user && (
+            <React.Fragment>
+              <NavLink className='nav-item nav-link' to='/billing'>
+                Billing
+              </NavLink>
+              <NavLink className='nav-item nav-link' to='/forum'>
+                Forum
+              </NavLink>
+              <NavLink className='nav-item nav-link' to='/complaint'>
+                Complaint
+              </NavLink>
+              <NavLink className='nav-item nav-link' to='/fundraising'>
+                Fundraising
+              </NavLink>
+              <NavLink className='nav-item nav-link' to='/advertisement'>
+                Advertisement
+              </NavLink>
+              <NavLink className='nav-item nav-link' to='/profile'></NavLink>
 
-					{!user && (
-						<React.Fragment>
-							<NavLink className='nav-item nav-link' to='/login'>
-								Login
-							</NavLink>
-							<NavLink className='nav-item nav-link' to='/register'>
-								Register
-							</NavLink>
-						</React.Fragment>
-					)}
-					{user && (
-						<React.Fragment>
-							<NavLink className='nav-item nav-link' to='/billing'>
-								Billing
-							</NavLink>
-							<NavLink className='nav-item nav-link' to='/forum'>
-								Forum
-							</NavLink>
-							<NavLink className='nav-item nav-link' to='/complaint'>
-								Complaint
-							</NavLink>
-							<NavLink className='nav-item nav-link' to='/fundraising'>
-								Fundraising
-							</NavLink>
-							<NavLink className='nav-item nav-link' to='/advertisement'>
-								Advertisement
-							</NavLink>
-							<NavLink className='nav-item nav-link' to='/profile'></NavLink>
+              <NavDropdown
+                title={
+                  <img
+                    src={user.imageUrl}
+                    width='25'
+                    height='25'
+                    className='avatar d-inline-block align-top'
+                    alt='React Bootstrap logo'
+                  />
+                }
+                id='basic-nav-dropdown'
+              >
+                <NavDropdown.Item href='#action/3.1'>
+                  Logged in as:
+                  <br />
+                  {user.name}
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
 
-							<NavDropdown
-								title={
-									<img
-										src={user.imageUrl}
-										width='25'
-										height='25'
-										className='avatar d-inline-block align-top'
-										alt='React Bootstrap logo'
-									/>
-								}
-								id='basic-nav-dropdown'
-							>
-								<NavDropdown.Item href='#action/3.1'>
-									Logged in as:
-									<br />
-									{user.name}
-								</NavDropdown.Item>
-								<NavDropdown.Divider />
-
-								<NavLink
-									className='nav-item nav-link ml-3 dropdown-nav-item'
-									to='/dashboard'
-								>
-									Dashboard
-								</NavLink>
-								<NavDropdown.Divider />
-								<NavLink
-									className='nav-item nav-link ml-3 dropdown-nav-item'
-									to='/logout'
-								>
-									Logout
-								</NavLink>
-							</NavDropdown>
-						</React.Fragment>
-					)}
-				</div>
-			</div>
-		</nav>
-	);
+                <NavLink
+                  className='nav-item nav-link ml-3 dropdown-nav-item'
+                  to='/dashboard'
+                >
+                  Dashboard
+                </NavLink>
+                <NavDropdown.Divider />
+                <NavLink
+                  className='nav-item nav-link ml-3 dropdown-nav-item'
+                  to='/logout'
+                >
+                  Logout
+                </NavLink>
+              </NavDropdown>
+            </React.Fragment>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
