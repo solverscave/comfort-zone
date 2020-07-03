680fb5ffa5c2b504a3c233bbbf5a8dd361de968d
Muhammad Osama <imuhammadosama@outlook.com>
2020-07-03 18:24:11 +0500

    App named changed to Client.

--- client/src/index.js
+++ client/src/index.js
@@ -4,17 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-import App from './App';
+import Client from './client';
 import * as serviceWorker from './serviceWorker';
 import ScrollToTop from 'react-router-scroll-top';
 
 ReactDOM.render(
-	<BrowserRouter>
-		<ScrollToTop>
-			<App />
-		</ScrollToTop>
-	</BrowserRouter>,
-	document.getElementById('root')
+  <BrowserRouter>
+    <ScrollToTop>
+      <Client />
+    </ScrollToTop>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 serviceWorker.register();
