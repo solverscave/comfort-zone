a1c18df04896fda46811e2b4fca34163fd321df5
Muhammad Osama <imuhammadosama@outlook.com>
2020-07-17 22:54:18 +0500

    Thing got updated.

--- client/src/components/common/footer.jsx
+++ client/src/components/common/footer.jsx
@@ -15,9 +15,41 @@ export default function Footer() {
             discuss issues, post an ad or a funding compain.
           </p>
         </div>
-        <div className='col-4'>
+        <div className='col-4' style={{ textAlign: 'left' }}>
           <h3>Contact us</h3>
-          <p>Address: Housing Society, Rawalpindi</p>
+
+          <p style={{ color: 'white' }}>
+            <i class='fa fa-home'></i> Comfort Zone
+          </p>
+          <p>
+            <i class='fa fa-envelope'></i>{' '}
+            <a href='#' style={{ color: 'white' }}>
+              {' '}
+              admin@comfortzone.pk
+            </a>
+          </p>
+          <p style={{ color: 'white' }}>
+            <i class='fa fa-mobile'></i> +923073794329
+          </p>
+          <p>
+            <i class='fa fa-map-marker'></i> Airport Housing Society, Rawalpindi
+          </p>
+        </div>
+        <div className='col-4' style={{ textAlign: 'left' }}>
+          <h3>Location</h3>
+
+          <div>
+            <iframe
+              src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26588.23371162599!2d73.11005456305617!3d33.5915708158957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38dfec88546ee3e5%3A0x665cba7f5f2f0b08!2sAirport%20Housing%20Society%2C%20Rawalpindi%2C%20Punjab%2C%20Pakistan!5e0!3m2!1sen!2s!4v1594907315523!5m2!1sen!2s'
+              width='300'
+              height='150'
+              frameborder='0'
+              style={{ border: 0 }}
+              allowfullscreen=''
+              aria-hidden='false'
+              tabindex='0'
+            ></iframe>
+          </div>
         </div>
       </div>
     </footer>
--- client/src/components/pages/home.jsx
+++ client/src/components/pages/home.jsx
@@ -213,58 +213,7 @@ export default class Home extends Component {
               />
             </div>
           </div>
-          <div className='row landing-page-section-darker'>
-            <div className='col-6 align-self-center justify-content-center text-center'>
-              <img
-                src={require('../../assets/icons/icon-facility.svg')}
-                alt=''
-              />
-            </div>
-            <div className='col-6'>
-              <h1>Election System</h1>
-              <p className='text-justify'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                sollicitudin ante nisl, in feugiat odio rutrum at. Duis et lacus
-                sit amet risus sollicitudin tristique sed vitae metus.
-                Vestibulum ultricies ligula vitae porta blandit. Vestibulum
-                porta arcu sed lectus aliquam eleifend. Fusce sagittis eget elit
-                non euismod. Donec eu metus dictum, tempus odio nec, cursus
-                eros. Interdum et malesuada fames ac ante ipsum primis in
-                faucibus. Fusce tempor finibus magna. Nunc pharetra lectus at
-                turpis consequat, nec porttitor felis porta. Fusce nec quam
-                fermentum, cursus mauris ac, sollicitudin dui. Pellentesque eget
-                gravida nisl. Nunc quis mi rutrum, ornare leo eu, cursus ex.
-                Vivamus congue sapien eget urna auctor euismod. Aenean id nisi
-                in odio pretium maximus.
-              </p>
-            </div>
-          </div>
-          <div className='row landing-page-section-lighter'>
-            <div className='col-6'>
-              <h1>Tendor and Auction</h1>
-              <p className='text-justify'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                sollicitudin ante nisl, in feugiat odio rutrum at. Duis et lacus
-                sit amet risus sollicitudin tristique sed vitae metus.
-                Vestibulum ultricies ligula vitae porta blandit. Vestibulum
-                porta arcu sed lectus aliquam eleifend. Fusce sagittis eget elit
-                non euismod. Donec eu metus dictum, tempus odio nec, cursus
-                eros. Interdum et malesuada fames ac ante ipsum primis in
-                faucibus. Fusce tempor finibus magna. Nunc pharetra lectus at
-                turpis consequat, nec porttitor felis porta. Fusce nec quam
-                fermentum, cursus mauris ac, sollicitudin dui. Pellentesque eget
-                gravida nisl. Nunc quis mi rutrum, ornare leo eu, cursus ex.
-                Vivamus congue sapien eget urna auctor euismod. Aenean id nisi
-                in odio pretium maximus.
-              </p>
-            </div>
-            <div className='col-6 align-self-center justify-content-center text-center'>
-              <img
-                src={require('../../assets/icons/icon-tracking.svg')}
-                alt=''
-              />
-            </div>
-          </div>
+
           <div className='row landing-page-section-darker'>
             <div className='col-6 align-self-center justify-content-center text-center'>
               <img src={require('../../assets/icons/icon-ad.svg')} alt='' />
