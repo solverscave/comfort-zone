af3c1a7a74266c974460184d878f2fa3a8a94e4b
Muhammad Osama <imuhammadosama@outlook.com>
2020-07-22 18:19:24 +0500

    Assign button added to the complains.

--- client/src/components/complains.jsx
+++ client/src/components/complains.jsx
@@ -1,24 +1,42 @@
-import React, { Component } from "react";
-import { paginate } from "../utils/paginate";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer, toast } from "react-toastify";
-import Pagination from "./common/pagination";
-import ListGroup from "./common/listGroup";
-import { apiUrl } from "../config.json";
-const apiEndpoint = apiUrl + "/complains";
+import React, { Component } from 'react';
+import { paginate } from '../utils/paginate';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import Pagination from './common/pagination';
+import Popup from 'reactjs-popup';
+import Joi from 'joi-browser';
+import Form from './common/form';
+import ListGroup from './common/listGroup';
+import { apiUrl } from '../config.json';
+const apiEndpoint = apiUrl + '/complains';
 
 export default class Complains extends Component {
   state = {
     complains: [],
     status: [
-      { _id: 0, name: "All" },
-      { _id: 1, name: "Resolved!" },
-      { _id: 2, name: "Pending..." },
+      { _id: 0, name: 'All' },
+      { _id: 1, name: 'Resolved!' },
+      { _id: 2, name: 'Pending...' },
     ],
     pageSize: 5,
     currentPage: 1,
+    data: {
+      complainHandlerName: '',
+      complainHandlerPhone: '',
+    },
+  };
+
+  schema = {
+    complainHandlerName: Joi.string()
+      .required()
+      .min(10)
+      .label('complainHandlerName'),
+    complainHandlerPhone: Joi.string()
+      .required()
+      .min(20)
+      .label('complainHandlerPhone'),
   };
 
   async componentDidMount() {
@@ -36,10 +54,10 @@ export default class Complains extends Component {
     this.setState({ complains });
 
     try {
-      await axios.delete(apiEndpoint + "/" + complain._id);
-      toast.success("The complain was successfully deleted!");
+      await axios.delete(apiEndpoint + '/' + complain._id);
+      toast.success('The complain was successfully deleted!');
     } catch (ex) {
-      toast.error("Failed to delete the complain!");
+      toast.error('Failed to delete the complain!');
       this.setState({ complains: originalComplains });
     }
   };
@@ -57,17 +75,17 @@ export default class Complains extends Component {
     if (!this.state.complains.length)
       return (
         <div
-          className="align-self-center justify-content-center text-center"
-          style={{ padding: "150px" }}
+          className='align-self-center justify-content-center text-center'
+          style={{ padding: '150px' }}
         >
-          <img src={require("../assets/icons/loading.gif")} alt="" />
+          <img src={require('../assets/icons/loading.gif')} alt='' />
         </div>
       );
-    if (this.state.complains === "No complain was found")
+    if (this.state.complains === 'No complain was found')
       return (
         <h1
-          className="align-self-center justify-content-center text-center"
-          style={{ padding: "150px" }}
+          className='align-self-center justify-content-center text-center'
+          style={{ padding: '150px' }}
         >
           No complain was found!
         </h1>
@@ -86,9 +104,9 @@ export default class Complains extends Component {
 
     return (
       <React.Fragment>
-        <div className="row">
+        <div className='row'>
           <ToastContainer />
-          <div className="col-3">
+          <div className='col-3'>
             {
               <ListGroup
                 items={this.state.status}
@@ -97,21 +115,22 @@ export default class Complains extends Component {
               />
             }
           </div>
-          <div className="col-9">
-            <table className="table">
-              <thead style={{ color: "#fff", backgroundColor: " #159570" }}>
+          <div className='col-9'>
+            <table className='table'>
+              <thead style={{ color: '#fff', backgroundColor: ' #159570' }}>
                 <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Complains</th>
-                  <th scope="col">Posted by</th>
-                  <th scope="col">Status</th>
-                  <th scope="col">Delete</th>
+                  <th scope='col'>#</th>
+                  <th scope='col'>Complains</th>
+                  <th scope='col'>Posted by</th>
+                  <th scope='col'>Status</th>
+                  <th scope='col'>Assign</th>
+                  <th scope='col'>Delete</th>
                 </tr>
               </thead>
               <tbody>
                 {complains.map((complain) => (
                   <tr key={complain._id}>
-                    <th scope="row">{complains.indexOf(complain)}</th>
+                    <th scope='row'>{complains.indexOf(complain)}</th>
                     <td>
                       <Link to={`complain/${complain._id}`}>
                         {complain.title}
@@ -120,24 +139,57 @@ export default class Complains extends Component {
                     <td>
                       <img
                         src={complain.userImage}
-                        width="25"
-                        height="25"
-                        className="avatar d-inline-block align-top"
-                        alt="React Bootstrap logo"
+                        width='25'
+                        height='25'
+                        className='avatar d-inline-block align-top'
+                        alt='React Bootstrap logo'
                       />
-                      <span style={{ paddingLeft: "10px" }}>
+                      <span style={{ paddingLeft: '10px' }}>
                         <Link
-                          className="text-dark"
+                          className='text-dark'
                           to={`/profile/${complain.userId}`}
                         >
                           {complain.userName}
-                        </Link>{" "}
+                        </Link>{' '}
                       </span>
                     </td>
                     <td>{complain.status}</td>
                     <td>
+                      <Popup
+                        trigger={
+                          <button className='btn btn-cz'> Assign</button>
+                        }
+                        position='top center'
+                      >
+                        <h1>Apple</h1>
+                        {/* <form onSubmit={this.handleSubmit}>
+                          {this.renderInput(
+                            'title',
+                            'Title',
+                            'text',
+                            'Enter the title of the issue'
+                          )}
+                          {this.renderTextArea(
+                            'description',
+                            'Description',
+                            'text',
+                            'Enter the description of the issue'
+                          )}
+                          {this.renderSelect(
+                            'category',
+                            'Category',
+                            this.state.category
+                          )}
+                          <div className='row'>
+                            <div className='col-3'></div>
+                          </div>
+                          {this.renderButton('Submit')}
+                        </form> */}
+                      </Popup>
+                    </td>
+                    <td>
                       <button
-                        className="btn btn-danger"
+                        className='btn btn-danger'
                         onClick={() => this.handleDelete(complain)}
                       >
                         Delete
